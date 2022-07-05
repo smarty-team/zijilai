e2213dd7f33faab9f787c79d03622d33739a881b
josephxia <15906475@qq.com>
2022-07-05 19:28:07 +0800

    update

--- packages/ui/src/components.d.ts
+++ packages/ui/src/components.d.ts
@@ -3,13 +3,11 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {}
+export { }
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Card: typeof import('./components/Card.vue')['default']
-    LevelCard: typeof import('./components/LevelCard.vue')['default']
-    Navbar: typeof import('./components/Navbar.vue')['default']
+    Navbar: typeof import('./components/Card.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SideMenu: typeof import('./components/SideMenu.vue')['default']
